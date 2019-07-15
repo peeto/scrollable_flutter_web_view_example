4b53c1ca2b7e9b1dea4916ab9bb46f25c19e1dee
peeto <petersenc@gmail.com>
2019-07-15 19:58:48 +1000

    Solution for iOS overlay issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:overlay_container/overlay_container.dart';
 import 'package:responsive_container/responsive_container.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/foundation.dart';
 
 void main() => runApp(MyApp());
@@ -32,123 +32,126 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-
   @override
   Widget build(BuildContext context) {
 
     // Determine landscape or portrait
     Orientation currentOrientation = MediaQuery.of(context).orientation;
 
-    // Allow scroll gestures - See https://medium.com/flutter-io/the-power-of-webviews-in-flutter-a56234b57df2
-    /*
-    var verticalGestures = Factory<VerticalDragGestureRecognizer>(
-            () => VerticalDragGestureRecognizer());
-    var gestureSet = Set.from([verticalGestures]);
-    */
-    var gestureSet = null; // Except I can't get it to work
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
 
-      body:
-
-      ListView(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
 
-            Text('\r\n Karthik Ponnam is awesome\r\n'),
-
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://github.com/PonnamKarthik/FlutterWebView',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
+            // Overlay to fix iOS overlay bug
+            // without this iOS will display a webview over the container when scrolling up
+            OverlayContainer(
+                asWideAsParent: true,
+                show: true,
+                child: Container(
+                  height: 40.0,
+                  child: Text('Heading'),
                 )
             ),
 
-            Text('\r\n Emily Fortuna is awesome\r\n'),
-
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://medium.com/flutter-io/the-power-of-webviews-in-flutter-a56234b57df2',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
-                )
+            // Duplicate of above container to use as underlay and keep widget tree relatively intact
+            Container(
+              height: 40.0,
+              child: Text('Heading'),
             ),
 
-            Text('\r\n I\'m patching for AndroidX?\r\n'),
+            Expanded(
+                child: ListView(
+                  shrinkWrap: false,
+                  scrollDirection: Axis.vertical,
+                  physics: BouncingScrollPhysics(),
+                  children: <Widget>[
 
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://github.com/peeto/FlutterWebView/commit/7a43af67f1058719120c25ca416eff77de6a55eb',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
+                    Text('\r\n Karthik Ponnam is awesome\r\n'),
+
+                    // For a WebView to work within a ListView it needs it's size set
+                    // Use a ResponsiveContainer to set relative sizes
+                    ResponsiveContainer(
+                        widthPercent: 100,
+                        heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
+                        alignment: Alignment(0, 0),
+                        margin: EdgeInsets.all(0),
+                        padding: EdgeInsets.all(0),
+                        child: WebView(
+                          initialUrl: 'https://github.com/PonnamKarthik/FlutterWebView',
+                          javascriptMode: JavascriptMode.unrestricted,
+                        )
+                    ),
+
+                    Text('\r\n Emily Fortuna is awesome\r\n'),
+
+                    ResponsiveContainer(
+                        widthPercent: 100,
+                        heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
+                        alignment: Alignment(0, 0),
+                        margin: EdgeInsets.all(0),
+                        padding: EdgeInsets.all(0),
+                        child: WebView(
+                          initialUrl: 'https://medium.com/flutter-io/the-power-of-webviews-in-flutter-a56234b57df2',
+                          javascriptMode: JavascriptMode.unrestricted,
+                        )
+                    ),
+
+                    Text('\r\n Google\r\n'),
+
+                    ResponsiveContainer(
+                        widthPercent: 100,
+                        heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
+                        alignment: Alignment(0, 0),
+                        margin: EdgeInsets.all(0),
+                        padding: EdgeInsets.all(0),
+                        child: WebView(
+                          initialUrl: 'https://google.com',
+                          javascriptMode: JavascriptMode.unrestricted,
+                        )
+                    ),
+
+                    Text('\r\n Me\r\n'),
+
+                    ResponsiveContainer(
+                        widthPercent: 100,
+                        heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
+                        alignment: Alignment(0, 0),
+                        margin: EdgeInsets.all(0),
+                        padding: EdgeInsets.all(0),
+                        child: WebView(
+                          initialUrl: 'https://peeto.net',
+                          javascriptMode: JavascriptMode.unrestricted,
+                        )
+                    ),
+
+                    Text('\r\n I can\'t get Emily Fortuna\'s scrolling working\r\n'),
+
+                    ResponsiveContainer(
+                        widthPercent: 100,
+                        heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
+                        alignment: Alignment(0, 0),
+                        margin: EdgeInsets.all(0),
+                        padding: EdgeInsets.all(0),
+                        child: WebView(
+                          initialUrl: 'https://www.youtube.com/watch?v=5cc_h5Ghuj4',
+                          javascriptMode: JavascriptMode.unrestricted,
+                        )
+                    ),
+
+                  ],
                 )
-            ),
-
-            Text('\r\n Google\r\n'),
-
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://google.com',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
-                )
-            ),
-
-            Text('\r\n Me\r\n'),
-
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://peeto.net',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
-                )
-            ),
-
-            Text('\r\n I can\'t get Emily Fortuna\'s scrolling working\r\n'),
-
-            ResponsiveContainer(
-                widthPercent: 100,
-                heightPercent: currentOrientation == Orientation.portrait ? 30 : 60,
-                alignment: Alignment(0, 0),
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(0),
-                child: WebView(
-                  initialUrl: 'https://www.youtube.com/watch?v=5cc_h5Ghuj4',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  gestureRecognizers: gestureSet,
-                )
-            ),
+            )
 
           ],
         )
+
     );
   }
 }
